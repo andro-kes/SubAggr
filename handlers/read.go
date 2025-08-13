17ab54766b16a6d44d18da65754f2114664bb073
Andrey Denisov <and.denisov26@gmail.com>
2025-08-13 18:58:53 +0300

    Added handlers

--- handlers/read.go
+++ handlers/read.go
@@ -1,2 +1,7 @@
 package handlers
 
+import "github.com/gin-gonic/gin"
+
+func ReadNote(c *gin.Context) {
+	
+}
--- utils/err.go
+++ utils/err.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func MustNotError(err error, msg string) {
@@ -13,6 +15,20 @@ func MustNotError(err error, msg string) {
 func IsValid(ok bool, msg string) bool {
 	if !ok {
 		log.Println(msg)
+		return false
+	}
+	return true
+}
+
+/*
+Проверяет, есть ли ошибка при работе хэндлера.
+true - ошибки нет,
+false - есть ошибка
+*/
+func CheckError(c *gin.Context, err error, msg string) bool {
+	if err != nil {
+		log.Println(msg)
+		c.JSON(400, gin.H{"Error": err.Error})
 		return false
 	}
 	return true
